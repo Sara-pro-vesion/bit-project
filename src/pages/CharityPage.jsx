32414cfb8afe427694fbe5b82ebf949339185f22
Sara <kr.sara.prv@gmail.com>
2026-04-13 22:52:15 +0200

    fetching completed

--- src/pages/CharityPage.jsx
+++ src/pages/CharityPage.jsx
@@ -5,6 +5,7 @@ import Marketplace from "../components/Market";
 import Footer from "../components/Footer";
 
 export default function CharityPage({ donations, onCreateClaim }) {
+  
   return (
     <div>
       <div className="flex flex-col gap-20 md:gap-[120px]">
--- src/services/api.js
+++ src/services/api.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
 
 const api = axios.create({
-  //the base URL is /api/v1 [cite: 114]
+
   baseURL: 'http://localhost:5000/api/v1', 
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// This interceptor automatically adds the JWT token to every request
+
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
